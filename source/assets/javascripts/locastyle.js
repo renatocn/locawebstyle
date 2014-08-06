66af13509152a0a7c94abfcb78bc4e9db5680450
Renato Nitta <renatocn@gmail.com>
2014-08-06 18:33:24 -0300

    organize js manifest

--- source/assets/javascripts/locastyle.js
+++ source/assets/javascripts/locastyle.js
@@ -1,3 +1,4 @@
+// included dependencies
 //= require libs/_hopscotch
 //= require libs/_jquery.cookie
 //= require libs/jquery.mask
@@ -5,27 +6,31 @@
 //= require libs/pikaday
 //= require libs/pikaday.jquery
 
+// templates
 //= require locastyle/templates/_popover
 //= require locastyle/templates/_modal
 //= require locastyle/templates/_dropdown
 
+// basic
 //= require locastyle/_initializer
 //= require locastyle/_breakpoint-check
 //= require locastyle/_sidebars
-//= require locastyle/_collapse
-//= require locastyle/_modal
 //= require locastyle/_dismiss
-//= require locastyle/_popover
-//= require locastyle/_dropdown
-//= require locastyle/_tabs
 //= require locastyle/_templates
 //= require locastyle/_general
-//= require locastyle/_progress-bar
 //= require locastyle/_form
 //= require locastyle/_guided-tour
-//= require locastyle/_custom-fields
 //= require locastyle/_topbar-curtain
 //= require locastyle/_btn-group
 //= require locastyle/_datepicker
 //= require locastyle/_char-counter
+
+// custom modules
+//= require locastyle/_collapse
+//= require locastyle/_modal
+//= require locastyle/_popover
+//= require locastyle/_dropdown
+//= require locastyle/_tabs
+//= require locastyle/_progress-bar
+//= require locastyle/_custom-fields
 //= require locastyle/_track-events
